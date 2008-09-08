5d48e558e99a7f8e84fdf973fdd7d8c777bd41b0
Tekkub <tekkub@gmail.com>
2008-09-08 15:08:27 -0600

    eXPeritia:New() is totally wasteful

--- core.lua
+++ core.lua
@@ -14,57 +14,55 @@ defaults = {
 eXPeritia:SetPoint("TOP", UIParent, "TOP", 0, -100)	-- Position of bar
 --[[ Configuration end ]]
 
-local LargeValue = function(value) 
+local LargeValue = function(value)
 	if(value > 999 or value < -999) then
-		return string.format("|cffffffff%.0f|rk", value / 1e3) 
+		return string.format("|cffffffff%.0f|rk", value / 1e3)
 	else
 		return "|cffffffff"..value.."|r"
-	end 
-end 
-
-function eXPeritia:New()
-	local bgleft = self:CreateTexture(nil, "BACKGROUND")
-	bgleft:SetTexture([[Interface\AddOns\eXPeritia\bg-left]])
-	bgleft:SetPoint("TOPLEFT", -4, 0)
-	bgleft:SetPoint("BOTTOMRIGHT", self, "BOTTOM", 0, 0)
-	self.bgLeft = bgleft
-	
-	local bgright = self:CreateTexture(nil, "BACKGROUND")
-	bgright:SetTexture([[Interface\AddOns\eXPeritia\bg-right]])
-	bgright:SetPoint("TOPRIGHT", 4, 0)
-	bgright:SetPoint("BOTTOMLEFT", self, "BOTTOM", 0, 0)
-	self.bgRight = bgright
-	
-	self.Indicator = self:NewIndicator(1)
-	self.Last = self:NewIndicator(1)
-	self.Rest = self:NewIndicator(1)
-	
-	local textPercent = self:CreateFontString(nil, "OVERLAY")
-	textPercent:SetFont("Fonts\\FRIZQT__.TTF", 14)
-	textPercent:SetPoint("LEFT", self.Indicator, 10, 0)
-	textPercent:SetShadowOffset(1, -1)
-	self.textPercent = textPercent
-	
-	local textAbsolute = self:CreateFontString(nil, "OVERLAY")
-	textAbsolute:SetFont("Fonts\\FRIZQT__.TTF", 10)
-	textAbsolute:SetPoint("BOTTOMRIGHT", self, "TOPRIGHT", 0, 0)
-	textAbsolute:SetShadowOffset(1, -1)
-	self.textAbsolute = textAbsolute
-	
-	local textNeeded = self:CreateFontString(nil, "OVERLAY")
-	textNeeded:SetFont("Fonts\\FRIZQT__.TTF", 10)
-	textNeeded:SetPoint("TOPRIGHT", self, "BOTTOMRIGHT", 0, 0)
-	textNeeded:SetShadowOffset(1, -1)
-	self.textNeeded = textNeeded
-	
-	self:SetMovable(true)
-	self:SetScript("OnEvent", self.Update)
-	self:RegisterEvent("PLAYER_XP_UPDATE")
-	self:RegisterEvent("PLAYER_LEVEL_UP")
-	self:RegisterEvent("PLAYER_LOGIN")
-	
-	self:Hide()
+	end
 end
+
+local bgleft = eXPeritia:CreateTexture(nil, "BACKGROUND")
+bgleft:SetTexture([[Interface\AddOns\eXPeritia\bg-left]])
+bgleft:SetPoint("TOPLEFT", -4, 0)
+bgleft:SetPoint("BOTTOMRIGHT", eXPeritia, "BOTTOM", 0, 0)
+eXPeritia.bgLeft = bgleft
+
+local bgright = eXPeritia:CreateTexture(nil, "BACKGROUND")
+bgright:SetTexture([[Interface\AddOns\eXPeritia\bg-right]])
+bgright:SetPoint("TOPRIGHT", 4, 0)
+bgright:SetPoint("BOTTOMLEFT", eXPeritia, "BOTTOM", 0, 0)
+eXPeritia.bgRight = bgright
+
+eXPeritia.Indicator = eXPeritia:NewIndicator(1)
+eXPeritia.Last = eXPeritia:NewIndicator(1)
+eXPeritia.Rest = eXPeritia:NewIndicator(1)
+
+local textPercent = eXPeritia:CreateFontString(nil, "OVERLAY")
+textPercent:SetFont("Fonts\\FRIZQT__.TTF", 14)
+textPercent:SetPoint("LEFT", eXPeritia.Indicator, 10, 0)
+textPercent:SetShadowOffset(1, -1)
+eXPeritia.textPercent = textPercent
+
+local textAbsolute = eXPeritia:CreateFontString(nil, "OVERLAY")
+textAbsolute:SetFont("Fonts\\FRIZQT__.TTF", 10)
+textAbsolute:SetPoint("BOTTOMRIGHT", eXPeritia, "TOPRIGHT", 0, 0)
+textAbsolute:SetShadowOffset(1, -1)
+eXPeritia.textAbsolute = textAbsolute
+
+local textNeeded = eXPeritia:CreateFontString(nil, "OVERLAY")
+textNeeded:SetFont("Fonts\\FRIZQT__.TTF", 10)
+textNeeded:SetPoint("TOPRIGHT", eXPeritia, "BOTTOMRIGHT", 0, 0)
+textNeeded:SetShadowOffset(1, -1)
+eXPeritia.textNeeded = textNeeded
+
+eXPeritia:SetMovable(true)
+eXPeritia:SetScript("OnEvent", eXPeritia.Update)
+eXPeritia:RegisterEvent("PLAYER_XP_UPDATE")
+eXPeritia:RegisterEvent("PLAYER_LEVEL_UP")
+eXPeritia:RegisterEvent("PLAYER_LOGIN")
+
+eXPeritia:Hide()
 
 function eXPeritia:ApplyOptions()
 	if(not eXPeritiaDB) then eXPeritiaDB = defaults else defaults = nil end
@@ -86,7 +84,7 @@ function eXPeritia:ApplyOptions()
 	self.textPercent:SetTextColor(color.r, color.g, color.b)
 	self.textAbsolute:SetTextColor(color.r, color.g, color.b)
 	self.textNeeded:SetTextColor(color.r, color.g, color.b)
-	
+
 	self:SetWidth(self.db.Width)
 	self:SetHeight(self.db.Height)
 
@@ -115,7 +113,7 @@ function eXPeritia:Update(event)
 	else
 		self.Rest:Hide()
 	end
-	
+
 	self.textPercent:SetFormattedText("|cffffffff%.1f|r%%", min/max*100)
 	self.textAbsolute:SetText(LargeValue(min-max))
 	if(last and last ~= min) then
@@ -123,7 +121,7 @@ function eXPeritia:Update(event)
 		self.textNeeded:SetFormattedText("|cffffffff%.0f|rx", (max-min)/(min-last))
 	end
 	if(last ~= min) then last = min end
-	
+
 	if(event~="noFade" and not self:IsShown()) then
 		fadeIn.fadeTimer = 0
 		fadeIn.finishedFunc = StartFadingOut
@@ -142,5 +140,3 @@ function eXPeritia:Move(ind, percent)
 	ind:ClearAllPoints()
 	ind:SetPoint("TOPLEFT", self.db.Width*percent, 0)
 end
-
-eXPeritia:New()
